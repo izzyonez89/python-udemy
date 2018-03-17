4da7099055b6ba5e99aa21df9650f6e827b145da
izzyonez89 <izzy189@gmail.com>
2018-03-17 18:08:14 +0800

    First Commit

---
+++ index.py
@@ -0,0 +1,29 @@
+import sys
+# import sys untuk import system
+print(sys.version)
+#Addition
+2+3
+#Subtraction
+6-4
+#Division. Always return a float
+20/4
+#Multiplication
+6*7
+#Parentheses
+(3+5)/2
+#Normal division
+21/4
+#Floor divison. Fractional part in discarded
+21//4
+#Remainder (baki)
+21%4
+#Power(kuasa)
+2**4
+#Mixing integers and floating point
+3*8.75-2
+#Assigning 5 to the variable no_rows
+no_rows=5
+no_seats_per_row=10
+no_rows*no_seats_per_row
+#This will cause an error as we have not assigned a value
+m
